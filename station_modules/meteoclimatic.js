f4482854723b7d3f07812df9e22b6f1c9f00e364
voliva <olivarra1@gmail.com>
2016-05-09 00:02:57 +0200

    ESCAT Stations

--- station_modules/meteoclimatic.js
+++ station_modules/meteoclimatic.js
@@ -3,14 +3,30 @@ var Q = require('q');
 var Data = require("./../models/data");
 
 var stations = [];
-stations.push({code: "stcarlesrapita", arg: {region: "ESCAT", code: "ESCAT4300000043540A"}});
+stations.push({code: "meteoclimatic/stcarlesrapita", arg: {region: "ESCAT", code: "ESCAT4300000043540A"}});
+stations.push({code: "meteoclimatic/miamiplatja", arg: {region: "ESCAT", code: "ESCAT4300000043892C"}});
+stations.push({code: "meteoclimatic/hospitalet", arg: {region: "ESCAT", code: "ESCAT4300000043890A"}});
+stations.push({code: "meteoclimatic/cubelles", arg: {region: "ESCAT", code: "ESCAT0800000008880D"}}); // Nativa http://meteo.ea3hkb.com/
+stations.push({code: "meteoclimatic/vilanova", arg: {region: "ESCAT", code: "ESCAT0800000008800F"}}); // Nativa http://www.meteovilanova.info/Current_Vantage.htm
+stations.push({code: "meteoclimatic/castelldefels", arg: {region: "ESCAT", code: "ESCAT0800000008860B"}});
+stations.push({code: "meteoclimatic/barcelona", arg: {region: "ESCAT", code: "ESCAT0800000008003F"}}); // Nativa http://meteo.cmima.csic.es/davis/Current_Vantage_Pro_Plus.html
+stations.push({code: "meteoclimatic/premiaport", arg: {region: "ESCAT", code: "ESCAT0800000008330B"}}); // Nativa http://www.meteopremia.com/port/
+stations.push({code: "meteoclimatic/premiaplatja", arg: {region: "ESCAT", code: "ESCAT0800000008330E"}}); // Nativa http://cbc117.zapto.org/
+stations.push({code: "meteoclimatic/mataro", arg: {region: "ESCAT", code: "ESCAT0800000008301F"}});
+stations.push({code: "meteoclimatic/calella", arg: {region: "ESCAT", code: "ESCAT0800000008370A"}});
+stations.push({code: "meteoclimatic/ciutadellamenorca", arg: {region: "ESIBA", code: "ESIBA0700000307760A"}});
+
+
+// premia cabrera mataro canet calella
+// TODO Awekas: (premià http://www.awekas.at/es/instrument.php?id=12238)
+
 
 var MCData = {};
 function getMCData(region){
   var deferred = new Q.defer();
 
   // Assuming 1 thread!
-  if(MCData[region]) return MCData;
+  if(MCData[region]) return MCData[region];
   MCData[region] = deferred.promise;
 
 	lwutils.getHTML("www.meteoclimatic.net", "/mapinfo/" + region, {
