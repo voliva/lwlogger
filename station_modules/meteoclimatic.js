3979b48caa5edacadeb278f905042dcc622489b9
voliva <olivarra1@gmail.com>
2016-05-08 16:45:09 +0200

    Parse meteoclimatic rows

--- station_modules/meteoclimatic.js
+++ station_modules/meteoclimatic.js
@@ -3,7 +3,7 @@ var Q = require('q');
 var Data = require("./../models/data");
 
 var stations = [];
-stations.push({code: "mctest", arg: {region: "ESCAT", code: "Whatever"}});
+stations.push({code: "mctest", arg: {region: "ESCAT", code: "ESCAT0800000008915A"}});
 
 var MCData = {};
 function getMCData(region){
@@ -13,10 +13,13 @@ function getMCData(region){
   if(MCData[region]) return MCData;
   MCData[region] = deferred.promise;
 
-	lwutils.getHTML("www.meteoclimatic.net", "/mapinfo/" + region).then(function(html){
+	lwutils.getHTML("www.meteoclimatic.net", "/mapinfo/" + region, {
+    "Accept-Language": "ca-ES"
+  }).then(function(html){
     deferred.resolve(new (lwutils.splitter)(html)
       .cropToStrEx("<!-- Calendarii -->")
-      .getToStrEx("amb segell Meteoclimatic de qualitat destacada"));
+      .getToStrEx("amb segell Meteoclimatic de qualitat destacada")
+      .getString());
 	});
 
 	return MCData[region];
@@ -25,10 +28,30 @@ function getMCData(region){
 function fetcher(args, timezone){
   var region = args.region;
   var code = args.code;
-  console.log("Fetch");
 
-  return getMCData(region).then(function(splitter){
-    console.log(splitter.getString());
+  return getMCData(region).then(function(html){
+    html = (new (lwutils.splitter)(html))
+      .cropToStrEx("/perfil/" + code)
+      .getToStrEx("</tr>")
+      .getString();
+
+    if(html.indexOf("background: #FFADAD;") >= 0) return null; // >1.5h lag
+    if(html.indexOf("background: #FFFFB9;") >= 0) return null; // >20min lag
+
+    var captureRegexp = /<td.*?>(.*?)<\/td/g;
+    var i = 0;
+    var res;
+    while(res = captureRegexp.exec(html)){
+      console.log(i, res[1]);
+      i++;
+    }
+
+    /* Now it's col 10
+    var windCaptureRegexp = />([0-9]+)&nbsp;([NSEW]+)</g;
+    res = windCaptureRegexp.exec(html);
+    if(res){
+      console.log("Wind", res[1], "Dir", res[2]);
+    }*/
   });
 }
 
